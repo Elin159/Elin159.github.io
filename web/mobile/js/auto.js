2dbcddbab525118a6fdc46d96f476a25f20fbe04
huanan <13640339846@163.com>
2019-09-09 16:17:06 +0800

    [+]调整mobile导航功能，提升js执行顺序

--- web/mobile/js/auto.js
+++ web/mobile/js/auto.js
@@ -1,9 +1,10 @@
-;(function (win, doc) {
+; (function (win, doc) {
     var docEl = doc.documentElement,
         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
     var init = function () {
-        var windowWidth = docEl.clientWidth
-        if (!windowWidth) return
+        var windowWidth = docEl.clientWidth;
+        if (!windowWidth) return;
+        // windowWidth = windowWidth < 320 ? 320 : windowWidth;
         docEl.style.fontSize = windowWidth / 7.5 + 'px'
     }
     if (!doc.addEventListener) return
